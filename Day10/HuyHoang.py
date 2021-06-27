eb931cf61c73273cd9fb419b9c5d466225d8fe5f
Hoàng Nguyễn Huy <hoang@gmail.com>
2021-06-27 21:06:46 +0700

    bt

--- Day10/HuyHoang.py
+++ Day10/HuyHoang.py
@@ -8,7 +8,7 @@ class KhachHangMoMo:
     def money_free_for_open_account(sefl):
         server = Server()
         dangnhap = server.login(sefl.username, sefl.password)
-        if
+        
 
     def with_draw(self, amount):
         pass
@@ -27,5 +27,35 @@ class CongDan:
     def set_name(self, name):
         self.__name = name
 
-    def set_name(self, id):
+    def set_id(self, id):
         self.__id = id
+
+if __name__=='__main__':
+    congdan = CongDan("huyhoang", 1234)
+    congdan.set_name("huy")
+    congdan.set_id(12)
+    name = congdan.get_name()
+    id = congdan.get_id()
+    print(name, id)
+
+class CongAn(CongDan):
+    def __init__(self, name, id, cap_bac):
+        super().__init__(name, id, cap_bac)
+        self.__cap_bac = cap_bac
+
+    def get_cap_bac(self):
+        return self.__cap_bac
+
+class BacSi(CongDan):
+    def __init__(self, name, id, chuyen_khoa):
+        super().__init__(name, id, chuyen_khoa)
+        self.__chuyen_khoa = chuyen_khoa 
+
+    def get_chuyen_khoa(self):
+        return self.__chuyen_khoa
+    
+    def get_name_and_id(self):
+        {
+            "name" = self.__name
+            "id"   = self.__id
+        }
