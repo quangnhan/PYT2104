56d5121191bd26275dfed88599b76a462085e210
unknown <thao@gmail.com>
2021-06-27 21:26:33 +0700

    Add file

--- Day10/thao.py
+++ Day10/thao.py
@@ -1,28 +1,27 @@
 
 from server import Server
-class KhachHangMoMo:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.account_balance = 0
-    def money_free_for_open_account(self):
-        server = Server()
-        LoginSuccess = server.login(self.username, self.password)
-        if LoginSuccess = True:
-            print("Login thanh cong!")
-            self.account_balance = account_balance
-        else:
-            self.account_balance +=50
-            print("So du hien tai:"{self.account_balance})
-    def with_draw(self, amount):
-        if (amount > self.account_balance):
-            print ("Lỗi")
+# class KhachHangMoMo:
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+#         self.account_balance = 0
+#     def money_free_for_open_account(self):
+#         server = Server()
+#         LoginSuccess = server.login(self.username, self.password)
+#         if LoginSuccess = True:
+#             print("Login thanh cong!")
+#             self.account_balance = account_balance
+#         else:
+#             self.account_balance +=50
+#             print("So du hien tai:"{self.account_balance})
+#     def with_draw(self, amount):
+#         if (amount > self.account_balance):
+#             print ("Lỗi")
 
 class CongDan:
-    def __init__(self, name, id, key):
+    def __init__(self, name, id):
         self.__name = name
         self.__id = id
-        self.__key = key
     def get_name (self):
         return self.__name
     def get_id (self):
@@ -33,10 +32,10 @@ class CongDan:
         self.__id = id
 
 class CongAn(CongDan):
-   def __init__(self, name, id, cap_bac):
+    def __init__(self, name, id, cap_bac):
        super().__init__(name, id)
        self.cap_bac = cap_bac
-    def get_capbac(self):
+    def get_cap_bac(self):
         return self.cap_bac
 
 class Bacsi(CongDan):
@@ -46,12 +45,16 @@ class Bacsi(CongDan):
     def get_chuyen_khoa(self):
         return self.chuyen_khoa
     def get_name_and_id(self):
-        return self.__name and self.__id
-
+        return {
+            "name" : self.get_name(),
+            "id" : self.get_id()
+        }
 
 if __name__ == "__main__":
     name = "Thao"
-    id = "id"
-    Thao = 
-    self.__name
+    id = "1234"
+    Thao = CongDan(name, id)
+    bacsi = Bacsi(name, id, 'Răng')
+    print(bacsi.get_chuyen_khoa())
+    print(bacsi.get_name_and_id())
 
