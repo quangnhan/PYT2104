cf4d81d3f5391f51b41f390b6108007ebf5cce1e
Thành Nguyễn <emailthanhnguyen19@gmail.com>
2021-06-27 19:27:41 +0700

    mod rowvain.py

--- Day10/rowvain.py
+++ Day10/rowvain.py
@@ -4,10 +4,18 @@ class KhachHangMoMo:
     def __init__(self,username, password):
         self.username = username
         self.password = password 
-        
+        self.acount_balance = 0
 
-    def money_free_for_open_account():
+    def money_free_for_open_account(self):
         server = Server()
-        server.login()
+        response = server.login(self.username,self.password)
+
+        if response["login"] == False:
+            print("ban chua co tai khoan")
+            self.acount_balance = 50
 
 
+
+
+    def with_draw(self, amount):
+        pass
