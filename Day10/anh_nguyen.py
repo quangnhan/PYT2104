12841dbdffcf2f705ecc79320a807615757db4f6
unknown <anhnv185@gmail.com>
2021-06-27 20:40:02 +0700

    add set_name set_id

--- Day10/anh_nguyen.py
+++ Day10/anh_nguyen.py
@@ -2,12 +2,18 @@ class CongDan:
     def __init__(self, name, id):
         self.__name = name
         self.__id = id
-        
+
     def get_name(self, name):
-        return self.name
+        return self.__name
 
     def get_id(self, id):
-        return self.id
+        return self.__id
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_id(self, id):
+        self.__id = id
 
 if __name__ == "__main__":
     name = "Nguyen Anh"
