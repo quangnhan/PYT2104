a7a413bf369754d55a7181c416ba70e6729e56cd
Thành Nguyễn <emailthanhnguyen19@gmail.com>
2021-06-27 19:06:34 +0700

    add rowvain.py

---
+++ Day10/rowvain.py
@@ -0,0 +1,11 @@
+from server import Server
+
+class KhachHangMoMo:
+    def __init__(self,username, passwork):
+        server = Server(username,passwork)
+        server.login()
+
+    def money_free_for_open_account():
+        if server.login == False:
+
+
