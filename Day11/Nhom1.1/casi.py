afef83af748557b9890732f5a530541a3c23e647
unknown <phamkhacson2000@gmail.com>
2021-06-30 21:17:05 +0700

    add

---
+++ Day11/Nhom1.1/casi.py
@@ -0,0 +1,11 @@
+class CaSi :
+
+    def __init__(self, name, sex):
+        self.name=name
+
+    def say_hi(self):
+        print(f"Hello {self.name}")
+        
+    def tha_thinh(self):
+        pass
+    
