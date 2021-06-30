d871fa52b9481410fcb8b1ec9e6068b8906d3557
unknown <phamkhacson2000@gmail.com>
2021-06-30 21:04:30 +0700

    Add

---
+++ Day11/Nhom1.1/hoahau.py
@@ -0,0 +1,12 @@
+class HoaHau :
+
+    def __init__(self, name, sex):
+        self.name=name
+
+    def say_hi(self):
+        print(f"Hello {self.name}")
+        
+    def check_snt(self,n):
+        pass
+    
+    
