0a25f5696e882e621e19c08654f94c0b3c8d0eac
starlight9389 <starlight.9389@gmail.com>
2021-06-30 19:08:51 +0000

    update vhung.py file

---
+++ Day11/vhung.py
@@ -0,0 +1,13 @@
+class JsonFile:
+    def __init__(self, path):
+        self.path = path
+    
+    def show(self):
+        f = open(self.path, 'r')
+        for line in f:
+            print(line)
+
+if __name__ == '__main__':
+    path = './data/data.txt'
+    json = JsonFile(path)
+    json.show()
