d1aa3e68dcac09c7f556476fb4b859fa495158d9
devdeptraino1 <tao.gboy@gmail.com>
2021-06-30 19:05:46 +0700

    olo

--- Day11/tien.py
+++ Day11/tien.py
@@ -1,1 +1,8 @@
 # Day 11 cua toi
+class JsonFile():
+    def __init__(self):
+        pass
+    def show(self):
+        path = f"{os.getcwd()}/Day11/data/data.txt"
+        f = open(path,'r')
+        return f.readlines()
