dcd9a3637471094d9a3462720f87d8821b9b722e
starlight9389 <starlight.9389@gmail.com>
2021-06-30 19:24:30 +0000

    update vhung.py

--- Day11/vhung.py
+++ Day11/vhung.py
@@ -1,15 +1,15 @@
 import os
 
 class JsonFile:
-    def __init__(self, path):
+    def __init__(self):
         self.path = path
     
-    def show(self):
-        f = open(self.path, 'r')
+    def show(self, path):
+        f = open(path, 'r')
         for line in f:
             print(line)
 
 if __name__ == '__main__':
     path = f'{os.getcwd()}/Day11/data/data.txt'
-    json = JsonFile(path)
-    json.show()
+    json = JsonFile()
+    json.show(path)
