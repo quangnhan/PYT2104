79ab40e6ee1e1742f970416d15418df8bd0e2d6a
Khan Phan <huyen@gmail.com>
2021-06-30 19:14:02 +0700

    add file

---
+++ Day11/huyen.py
@@ -0,0 +1,15 @@
+import os
+
+class JsonFile:
+    def __init__(self):
+        pass
+
+    def show(self, path):
+               
+        f = open(path, 'r')
+        for line in f:
+            print(line)
+
+if __name__ == '__main__':
+    json = JsonFile()
+    json.show(f"{os.getcwd()}/Day11/data/data.txt")
