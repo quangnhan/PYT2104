48de5a9c85d591faab5aaab591494407e85e1c6c
devdeptraino1 <tao.gboy@gmail.com>
2021-06-30 21:04:56 +0700

    fddf

---
+++ Day11/nhomtien/kieunu.py
@@ -0,0 +1,7 @@
+class KieuNu():
+    def __init__(self,name,height,weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+    def say_hi(self):
+        print(f'i am the fuckin kieu nu {self.name} cao {self.height} nang {self.weight}')
