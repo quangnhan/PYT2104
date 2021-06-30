306cf9872d201d75269875c389c1d876bb87718d
starlight9389 <starlight.9389@gmail.com>
2021-06-30 21:01:12 +0000

    add casi, nguoimau

---
+++ Day11/nhom3/nguoimau.py
@@ -0,0 +1,10 @@
+from Day11.nhom3.kieunu import KieuNu
+class NguoiMau(KieuNu):
+    def __init__(self, age):
+        pass
+    
+    def say_hi(self):
+        pass
+    
+    def tinh_lai_kep(self, money, years, interest):
+        pass
