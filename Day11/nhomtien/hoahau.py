c903ea35f743c2411ddd08c1703151aad08c10de
devdeptraino1 <tao.gboy@gmail.com>
2021-07-03 21:55:15 +0700

    fix

--- Day11/nhomtien/hoahau.py
+++ Day11/nhomtien/hoahau.py
@@ -1,9 +1,9 @@
 class HoaHau:
     def __init__(self, name, height, weight):
-        super().__init__(name, height, weight)
+        super().__init__(name, height, weight) #(ke thua lop nao day)
 
     def say_hi(self):
-        return self.say_hi()
+        return self.say_hi() # (keo nay tien ko lamf  duoc , no unserstand)
     
     def check_snt(self):
         pass
