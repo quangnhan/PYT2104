8ad790eb5fae598c5e2e85c69fa70089c138870f
unknown <anhnv185@gmail.com>
2021-07-04 19:37:14 +0700

    bai tap 01

---
+++ Day12/anh_nguyen/post.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+import requests
+import json
+
+url = "https://60e1a9545a5596001730f19b.mockapi.io/human"
+new_data = {
+    "name": "Anh Nguyen",
+    "age": 25,
+},
+
+response = requests.post(url,data = new_data)
+data = response.json()
+
+pprint(data)
