c4ae8ee7fee767a349a8b010e762cc4856be4897
devdeptraino1 <tao.gboy@gmail.com>
2021-07-04 19:57:59 +0700

    anhan

--- Day12/tien/get.py
+++ Day12/tien/get.py
@@ -1,1 +1,9 @@
 # get 
+import requests
+from pprint import pprint
+
+url = "https://60e1a9d05a5596001730f1aa.mockapi.io/humann"
+print('343')
+data = requests.get(url).json()
+print("duoi data")
+print(data)
