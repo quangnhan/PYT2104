b3b4b39ef0ed667de020680a206c9cf96710754d
starlight9389 <starlight.9389@gmail.com>
2021-07-07 20:02:49 +0000

    add vhung.py

---
+++ Day13/vhung.py
@@ -0,0 +1,29 @@
+import threading
+import requests
+from database import Database
+
+db = Database()
+human = db.get(1)
+list_mockapi = db.get_mockapis()
+count = 0
+threads = []
+
+def post_data(human, api):
+    global count
+    print("Start", api["name"])
+    response = requests.post(api['url'], data=human)
+    print("End", api["name"], response)
+    if response.status_code == 201:
+        name = api['name']
+        print(f'{name} ok!')
+        count += 1
+
+for api in list_mockapi:
+    t = threading.Thread(target=post_data, args=(human, api,))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+
+print(f'count {count}')
