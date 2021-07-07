23bf83ba2f3ba21b33b1ec6936443cebebf86497
devdeptraino1 <tao.gboy@gmail.com>
2021-07-07 20:37:45 +0700

    sdsd

--- Day13/tien.py
+++ Day13/tien.py
@@ -1,1 +1,33 @@
-Day 13 
+# Day 13 
+import requests
+import threading
+from datetime import datetime
+from database import Database
+
+db = Database()
+humans = db.get(1)
+
+list_mockapi = db.get_mockapis()
+count = 0
+
+
+def post_data(api,human):
+    global count
+    print("Start send data", human["id"])
+    response = requests.post(api,human)
+    if response.status_code == 201:
+        print("send data successfully")
+        count += 1 
+    print("End",human["id"],response)
+
+threads = []
+
+for api in list_mockapi:
+    t = threading.Thread(target=post_data,args=(api["url"],humans))
+    t.start()
+    threads.append(t)
+
+for  t in threads:
+    t.join()
+
+ptinr(f'count: {count}')
