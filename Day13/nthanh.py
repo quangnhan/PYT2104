81e830af823b3cf1bcc62ab3e3d83017dad5f2d5
Thành Nguyễn <emailthanhnguyen19@gmail.com>
2021-07-07 20:51:54 +0700

    push all db

--- Day13/nthanh.py
+++ Day13/nthanh.py
@@ -4,29 +4,56 @@ from datetime import datetime
 from database import Database
 
 #Database
+# db = Database()
+# human = db.get(1)
+# list_mockapi = db.get_mockapis()
+# start = datetime.now()
+# threads = []
+# count = 0
+
+# def post_data(url, name, human):
+#     global count 
+#     name = api["name"]
+#     print("Start", name)
+#     response = requests.post(url, data=human)
+#     print("End", name, response)
+#     if response.status_code == 201:
+#         count += 1
+#         print(name, "ok!")
+    
+# for api in list_mockapi:
+#     t = threading.Thread(target=post_data, args=(api["url"],api["name"], human))
+#     t.start()
+#     threads.append(t)
+
+# for t in threads:
+#     t.join()
+    
+# print(f"Count : {count} Done : {datetime.now() - start}")
+
+Database
 db = Database()
-human = db.get(1)
+humans = db.get()
 list_mockapi = db.get_mockapis()
+url = list_mockapi[8]["url"]
 start = datetime.now()
 threads = []
 count = 0
 
-def post_data(url, name, human):
+def post_data(url, human):
     global count 
-    name = api["name"]
-    print("Start", name)
+    print("Start", human["id"])
     response = requests.post(url, data=human)
-    print("End", name, response)
+    print("End", human["id"], response)
     if response.status_code == 201:
         count += 1
-        print(name, "ok!")
     
-for api in list_mockapi:
-    t = threading.Thread(target=post_data, args=(api["url"],api["name"], human))
+for human in humans:
+    t = threading.Thread(target=post_data, args=(url, human))
     t.start()
     threads.append(t)
 
 for t in threads:
     t.join()
-    
+
 print(f"Count : {count} Done : {datetime.now() - start}")
