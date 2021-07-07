ab6facc43f6b09a65df8e1db7e8ae7a103c20639
devdeptraino1 <tao.gboy@gmail.com>
2021-07-07 21:03:55 +0700

    dsd

--- Day13/tien.py
+++ Day13/tien.py
@@ -1,33 +1,67 @@
-# Day 13 
 import requests
 import threading
 from datetime import datetime
 from database import Database
 
 db = Database()
-humans = db.get(1)
+humans = db.get()
+my_url = "https://60e1a9d05a5596001730f1aa.mockapi.io/humann"
 
-list_mockapi = db.get_mockapis()
+threads = []
+
 count = 0
-
 
 def post_data(api,human):
     global count
     print("Start send data", human["id"])
     response = requests.post(api,human)
     if response.status_code == 201:
-        print("send data successfully")
-        count += 1 
-    print("End",human["id"],response)
+        print("successfully")
+        count += 1
+    print("end", human["id"],response)
 
-threads = []
-
-for api in list_mockapi:
-    t = threading.Thread(target=post_data,args=(api["url"],humans))
+for person in humans:
+    t = threading.Thread(target=post_data, args=(my_url,person))
     t.start()
     threads.append(t)
 
-for  t in threads:
+for t in threads:
     t.join()
 
-print(f'count: {count}')
+print(f"count : {count}")
+
+# Day 13 
+# import requests
+# import threading
+# from datetime import datetime
+# from database import Database
+
+# db = Database()
+# humans = db.get(1)
+
+
+
+# list_mockapi = db.get_mockapis()
+# count = 0
+
+
+# def post_data(api,human):
+#     global count
+#     print("Start send data", human["id"])
+#     response = requests.post(api,human)
+#     if response.status_code == 201:
+#         print("send data successfully")
+#         count += 1 
+#     print("End",human["id"],response)
+
+# threads = []
+
+# for api in list_mockapi:
+#     t = threading.Thread(target=post_data,args=(api["url"],humans))
+#     t.start()
+#     threads.append(t)
+
+# for  t in threads:
+#     t.join()
+
+# print(f'count: {count}')
