b9078021d60baf0f3cf1a77091eacb9c83d2d3af
= <=>
2021-07-11 20:27:19 +0700

    theanh

--- Day14/theanh.py
+++ Day14/theanh.py
@@ -23,38 +23,42 @@
 import requests
 from log import Log
 
-
 class Server:
 
     def __init__(self):
         self.__url = "https://60e1a9cc5a5596001730f1a8.mockapi.io/human"
         self.__log = Log("theanh")
 
-    def get_id(self, id):
+    def get_by_id(self, id):
         response = {}
-
         try:
-            check_int = isinstance(id, int)
-            print(check_int)
-            if not check_int:
-             response = requests.get(f"{self.__url}/{id}")
-            self.__log.log("INFO", "successful")
+            ok = id ==int(id)
+            if not ok:
+                raise ValueError
+            response = requests.get(f"{self.__url}/{int(id)}").json()
+            self.__log.log("INFO", "get successful")
+        except ValueError:
+            self.__log.log("ERROR", f"id {id} khong phai so")
         except:
-            self.__log.log("ERROR", f"id {id} fail")
-
+            self.__log.log("ERROR", "server fail")
         return response
+        
 
     def post(self, data):
         try:
+            if not data["age"] or not data["name"]:
+                raise Exception
             requests.post(self.__url, data=data)
-            self.__log.log("INFO", "successful")
+            self.__log.log("INFO", "post data successful")
+            print(data["age"], data["name"])
         except:
-            self.__log.log("ERROR", "fail")
+            self.__log.log("ERROR", "data fail")
 
 if __name__ == "__main__":
-    post_data = {
-        "name": "theanh",
-        "age": 21,
+    data = {
+        "age" : "21",
+        "name" : "Anh"
     }
     theanh = Server()
-    theanh.get_id("1")
+    theanh.get_by_id("1a")
+    theanh.post(data )
