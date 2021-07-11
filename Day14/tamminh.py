a0be608e79e2c5619a60819495898060cbcc4a68
MinhTT <tatamminh151196@gmail.com>
2021-07-11 19:48:22 +0700

    commit

--- Day14/tamminh.py
+++ Day14/tamminh.py
@@ -12,21 +12,35 @@ class Server:
         response = {}
 
         try:
-
             response = requests.get(f"{self.__url}/{int(id)}").json()
+
+            if response.size() == 0:
+                raise Exception
+
             self.__log.log("INFO", "get thanh cong")
-        except:
+        except ValueError:
             self.__log.log("ERROR", f"id {id} khong phai so")
+        except:
+            self.__log.log("ERROR", "server khong tra ve du lieu")
 
         return response
 
     def post(self, data):
         try:
+            print(data["age"], data["name"])
+            if not data["age"] or not data["name"]:
+                raise Exception
+
             requests.post(self.__url, data=data)
             self.__log.log("INFO", "post data thanh cong")
         except:
             self.__log.log("ERROR", "data khong hop le")
 
 if __name__ == "__main__":
+    data = {
+        "age" : "25",
+        "name" : "Minh"
+    }
     sv = Server()
     sv.get_by_id("1a")
+    sv.post(data)
