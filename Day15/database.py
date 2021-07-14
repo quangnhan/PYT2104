f732e4d9cc5f8608925cceb5723423ff11c46970
quangnhan145 <nhan@leadplus.net>
2021-07-14 21:37:38 +0700

    update class Database

--- Day15/database.py
+++ Day15/database.py
@@ -1,21 +1,31 @@
 #pip install mysql-connector-python
-
 import mysql.connector
 
-# database la schema
-myconn = mysql.connector.connect(host = "127.0.0.1", 
-                                    user = "root", 
-                                    passwd = "123456", 
-                                    database = "leadplus")
-   
-#tạo đối tượng cursor
-cur = myconn.cursor()
- 
-# select dữ liệu từ database
-cur.execute("SELECT * FROM customers")
+class Database:
+    def __init__(self):
+        # database la schema
+        self.myconn = mysql.connector.connect(host = "127.0.0.1", 
+                                            user = "root", 
+                                            passwd = "123456", 
+                                            database = "leadplus")
     
-# tìm nạp các hàng từ đối tượng con trỏ  
-result = cur.fetchall()
+   
+        #tạo đối tượng cursor
+        self.cur = self.myconn.cursor()
+    
+    def get_customer(self, id=None):
+        if id == None:
+            # select dữ liệu từ database
+            self.cur.execute("SELECT * FROM customers")
+                
+            # tìm nạp các hàng từ đối tượng con trỏ  
+            result = self.cur.fetchall()
+        
+        return result
 
-for x in result:
-    print(x); 
+if __name__ == "__main__":
+    db = Database()
+    customers = db.get_customer()
+
+    for customer in customers:
+        print(customer) 
