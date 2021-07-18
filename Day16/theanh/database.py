47c6b42ff8512594d17e6437a6f725cfeb7a9dec
= <=>
2021-07-18 19:51:52 +0700

    theanh

---
+++ Day16/theanh/database.py
@@ -0,0 +1,31 @@
+#pip install mysql-connector-python
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        # database la schema
+        self.myconn = mysql.connector.connect(host = "127.0.0.1", 
+                                            user = "root", 
+                                            passwd = "phuonganh1639@", 
+                                            database = "leadplus")
+    
+   
+        #tạo đối tượng cursor
+        self.cur = self.myconn.cursor()
+    
+    def get_customer(self, id=None):
+        if id == None:
+            # select dữ liệu từ database
+            self.cur.execute("SELECT * FROM customers")
+                
+            # tìm nạp các hàng từ đối tượng con trỏ  
+            result = self.cur.fetchall()
+        
+        return result
+
+if __name__ == "__main__":
+    db = Database()
+    customers = db.get_customer()
+
+    for customer in customers:
+        print(customer) 
