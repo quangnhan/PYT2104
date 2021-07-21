bff955957616731112d345acb4638bdd1a46f8f0
quangnhan145 <nhan@leadplus.net>
2021-07-21 20:43:26 +0700

    Create connection

--- Day17/nhan.py
+++ Day17/nhan.py
@@ -2,9 +2,9 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.jn5hm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client["sample_training"]
-col = db["customer"]
+col = db["grades"]
 
-doc = col.find({"name":"Nhan"})
+doc = col.find_one()
 
 for x in doc:
     print(x)
